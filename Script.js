5f4f2322c909fc968e25a44c9a14ac54c3b242ac
LyeMinDuk <lmd388222@gmail.com>
2024-12-03 09:47:27 +0700

    fifth update

--- Script.js
+++ Script.js
@@ -7,12 +7,12 @@ var Selection = () => {
     var selected = firstSelect.value;
     secondSelect.innerHTML = "";
     var luaChon = [];
-    if (selected == "doDai") luaChon = ["", "in => cm", "cm => in", "mile => km", "km => mile"];
+    if (selected == "doDai") luaChon = ["", "inch => cm", "cm => inch", "dặm => km", "km => dặm"];
     else if (selected == "khoiLuong") luaChon = ["", "lbs => kg", "kg => lbs"];
     else if (selected == "vanToc") luaChon = ["", "km/h => m/s", "m/s =>km/h"];
     else if (selected == "apSuat") luaChon = ["", "atm => Pa", "Pa => atm", "mmHg => Pa", "Pa => mmHg"];
-    else if (selected == "nangLuong") luaChon = ["", "J => cal", "cal => J"];
-    else if (selected == "congSuat") luaChon = ["", "HP => kW", "kW => HP"];
+    else if (selected == "nangLuong") luaChon = ["", "Jun => cal", "cal => Jun"];
+    else if (selected == "congSuat") luaChon = ["", "Mã lực => kW", "kW => Mã lực"];
     else if (selected == "nhietDo") luaChon = ["", "°C => °F", "°F => °C"];
     for (var i = 0; i < luaChon.length; i++) {
         var luaChonMoi = document.createElement("option");
@@ -27,14 +27,16 @@ var change = () => {
     console.log(dauVao);
     if (dauVao < 0) {
         output.innerHTML = "Giá trị không hợp lệ";
-    } else if (kieu == "in => cm") {
+    } else if (dauVao == "") {
+        output.innerHTML = "Chưa nhập giá trị";
+    } else if (kieu == "inch => cm") {
         output.innerHTML = `${dauVao * 2.34} cm`;
-    } else if (kieu == "cm => in") {
-        output.innerHTML = `${dauVao / 2.34} in`;
-    } else if (kieu == "mile => km") {
+    } else if (kieu == "cm => inch") {
+        output.innerHTML = `${dauVao / 2.34} inch`;
+    } else if (kieu == "dặm => km") {
         output.innerHTML = `${dauVao * 1.609344} km`;
-    } else if (kieu == "km => mile") {
-        output.innerHTML = `${dauVao / 1.609344} mile`;
+    } else if (kieu == "km => dặm") {
+        output.innerHTML = `${dauVao / 1.609344} dặm`;
     } else if (kieu == "kg => lbs") {
         output.innerHTML = `${dauVao * 2.20462262185} lbs`;
     } else if (kieu == "lbs => kg") {
@@ -51,20 +53,18 @@ var change = () => {
         output.innerHTML = `${dauVao * 133.322} Pa`;
     } else if (kieu == "Pa => mmHg") {
         output.innerHTML = `${dauVao / 133.322} mmHg`;
-    } else if (kieu == "HP => kW") {
+    } else if (kieu == "Mã lực => kW") {
         output.innerHTML = `${dauVao / 1.3410220888} kW`;
-    } else if (kieu == "kW => HP") {
-        output.innerHTML = `${dauVao * 1.3410220888} HP`;
-    } else if (kieu == "J => cal") {
+    } else if (kieu == "kW => Mã lực") {
+        output.innerHTML = `${dauVao * 1.3410220888} Mã lực`;
+    } else if (kieu == "Jun => cal") {
         output.innerHTML = `${dauVao / 4.1868} cal`;
-    } else if (kieu == "cal => J") {
-        output.innerHTML = `${dauVao * 4.1868} J`;
+    } else if (kieu == "cal => Jun") {
+        output.innerHTML = `${dauVao * 4.1868} Jun`;
     } else if (kieu == "°F => °C") {
         output.innerHTML = `${(dauVao - 32) * 5 / 9} °C`;
     } else if (kieu == "°C => °F") {
         output.innerHTML = `${dauVao * 1.8 + 32} °F`;
-    } else if (dauVao == "") {
-        output.innerHTML = "Chưa nhập giá trị";
     } else {
         output.innerHTML = "Chưa chọn kiểu quy đổi";
     }
